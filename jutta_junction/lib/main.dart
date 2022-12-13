a8463a248b56d6c0f3c5c95d58e1ff2af73760ba
ishwar46 <ishwarura@gmail.com>
2022-12-13 20:13:33 +0545

    Update main.dart

--- jutta_junction/lib/main.dart
+++ jutta_junction/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:jutta_junction/pages/login_page.dart';
 import 'package:jutta_junction/pages/signup_page.dart';
+import 'package:jutta_junction/pages/test/landingpage.dart';
+import 'package:jutta_junction/pages/test/login_new.dart';
+import 'pages/home_page.dart';
 
-void main() {
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const Myapp());
+// }
+
+void main(List<String> args) {
   runApp(const Myapp());
 }
 
@@ -16,12 +25,13 @@ class Myapp extends StatelessWidget {
       themeMode: ThemeMode.system,
       //theme: MyTheme.lightTheme(context),
       //darkTheme: MyTheme.darkTheme(context),
-      initialRoute: "/signup",
+      initialRoute: "/login",
       debugShowCheckedModeBanner: false,
       //initialRoute: MyRoutes.homeRoute,
       routes: {
-        "/": (context) => SignUpPage(),
-        MyRoutes.loginRoute: (context) => SignUpPage(),
+        "/": (context) => LandingPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        //MyRoutes.loginewRoute: (context) => LoginScreen(),
       },
     );
   }
@@ -31,4 +41,6 @@ class MyRoutes {
   static String loginRoute = "/login";
   static String homeRoute = "/home";
   static String signupRoute = "/signup";
+  static String loginewRoute = "/loginnew";
+  static String landingRoute = "/landing";
 }
