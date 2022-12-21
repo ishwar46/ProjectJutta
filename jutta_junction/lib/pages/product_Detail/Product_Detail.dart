3eb64fb8b2cb762930e5bb813975b20a220372dd
sijan <sijankasalawat3339@gmail.com>
2022-12-21 16:28:56 +0545

    adding stock and ratting

--- jutta_junction/lib/pages/product_Detail/Product_Detail.dart
+++ jutta_junction/lib/pages/product_Detail/Product_Detail.dart
@@ -14,15 +14,32 @@ class _Product_detailState extends State<Product_detail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[200],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.green[200],
+        elevation: 0.0,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Color.fromARGB(255, 255, 255, 255)),
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
+      
       body: Body(),
+      // bottomNavigationBar: CurvedNavigationBar(
+      //   backgroundColor: Color.fromARGB(255, 2, 5, 8),
+      //   items: <Widget>[
+      //     Icon(Icons.home),
+      //     Icon(Icons.chat),
+      //     Icon(Icons.shopping_cart),
+      //     Icon(
+      //       Icons.account_box,
+      //       size: 30,
+      //     ),
+      //   ],
+      //   onTap: (index) {
+      //     //Handle button ta
+      //   },
+      // ),
     );
   }
 }
--- jutta_junction/lib/pages/product_Detail/body.dart
+++ jutta_junction/lib/pages/product_Detail/body.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+// import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -17,20 +20,55 @@ class _BodyState extends State<Body> {
         Column(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(top: 5),
+              padding: EdgeInsets.only(top: 0),
               child: Text(
                 "Nike",
                 textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30,color: Colors.white),
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.black),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 0, bottom: 0),
+              child: Image.asset(
+                "assets/images/nikes.png",
+                height: 200,
+                width: 200,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RatingBar.builder(
+                  initialRating: 4,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+                Image.asset(
+                  "assets/images/greendot.png",
+                  height: 20,
+                  width: 20,
+                ),
+                Text("Stock"),
+              ],
+            ),
           ],
-          
-          
         ),
         Container(
-          margin: EdgeInsets.only(top: size.height * 0.4),
-          height: size.height * 0.45,
+          margin: EdgeInsets.only(top: size.height * 0.05),
+          height: size.height * 0.48,
 
           decoration: BoxDecoration(
               color: Color.fromARGB(255, 230, 162, 162),
