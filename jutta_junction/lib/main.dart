53bc6ab2a41e1f0f6a77c8732cd72cff58455c1a
prasnaaryal <prasna101@gmail.com>
2022-12-24 10:01:25 +0545

    firbase add

--- jutta_junction/lib/main.dart
+++ jutta_junction/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:jutta_junction/pages/login_page.dart';
 import 'package:jutta_junction/pages/themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const Myapp());
 }
 
