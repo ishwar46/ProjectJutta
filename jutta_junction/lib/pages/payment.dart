d1b7fe12d360522eeff9838cd441ffe8c970aeab
Roz Maharjan <rox.maharjan@gmail.com>
2023-01-05 15:53:47 +0545

    Initiliazing Payment

---
+++ jutta_junction/lib/pages/payment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+
+
+class Payment extends StatefulWidget {
+  const Payment ({Key? key}) : super(key: key);
+
+  @override
+  _PaymentState createState() => _PaymentState();
+}
+class  _PaymentState extends State<Payment> {}
