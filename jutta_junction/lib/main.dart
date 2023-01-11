ce36c7fc9514a700ee03ca95629f99b51ce8a0b4
sijan <sijankasalawat3339@gmail.com>
2023-01-11 21:57:49 +0545

    merging

--- jutta_junction/lib/main.dart
+++ jutta_junction/lib/main.dart
@@ -2,17 +2,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jutta_junction/pages/login_page.dart';
-<<<<<<< HEAD
-import 'package:firebase_core/firebase_core.dart';
-import 'package:jutta_junction/pages/signup_page.dart';
-
-void main() {
-=======
-<<<<<<< HEAD
-import 'package:jutta_junction/pages/onboarding_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:jutta_junction/pages/signup_page.dart';
+
 
 
 Future <void> main() async {
@@ -38,13 +31,9 @@ class Myapp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //initialRoute: MyRoutes.homeRoute,
       routes: {
-<<<<<<< HEAD
+
         "/": (context) => LoginPage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
-=======
-<<<<<<< HEAD
-        "/": (context) => Onboarding(),
-        MyRoutes.loginRoute: (context) => Onboarding(),
 
         "/register":(BuildContext context) =>RegPage(),
         // "/": (context) => LoginPage(),
