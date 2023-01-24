d48507e7eedc08a910154850d2fa85304baaa9cf
Roz Maharjan <rox.maharjan@gmail.com>
2023-01-24 10:59:52 +0545

    editing

--- jutta_junction/lib/pages/product_Detail/Product.dart
+++ jutta_junction/lib/pages/product_Detail/Product.dart
@@ -21,9 +21,9 @@ class Product {
   );
 }
 List<Product> product1=[
-  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/converse.png", name: "Converse", price: 1200,color: Colors.red),
-  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/converse.png", name: "Converse", price: 1200,color: Colors.red),
-  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/converse.png", name: "Converse", price: 1200,color: Colors.red),
-  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/converse.png", name: "Converse", price: 1200,color: Colors.red),
+  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/converse.png", name: "Converse", price: 3000,color: Colors.red),
+  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/adidaslogo.png", name: "Adidas", price: 5000,color: Colors.red),
+  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/jordan4.png", name: "JORDAN 4", price: 7000,color: Colors.red),
+  Product(id: 1, no: 11, discription: "jutta", image: "assets/images/drmartenslogo.png", name: "DrMartin", price: 5000,color: Colors.red),
 
 ];
