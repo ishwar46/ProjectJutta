51c096ffba083f2246acf67c256f82246f983b06
ishwar46 <ishwarura@gmail.com>
2023-02-11 08:48:00 +0545

    Create icon_test.dart

---
+++ jutta_junction/test/icon_test.dart
@@ -0,0 +1,22 @@
+//icon test
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jutta_junction/pages/login_page.dart';
+
+void main() {
+  testWidgets('Icon test', (WidgetTester tester) async {
+    // Build the icon widget
+    Widget icon = MaterialApp(
+      home: Scaffold(
+        body: Icon(Icons.add),
+      ),
+    );
+
+    // Add the icon to the tester
+    await tester.pumpWidget(icon);
+
+    // Verify that the icon is displayed
+    expect(find.byType(Icon), findsOneWidget);
+  });
+}
