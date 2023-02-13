6efd5e741cdbff90de72c2ab526e06b3535a6808
Kritika55 <94155352+Kritika55@users.noreply.github.com>
2023-02-13 13:09:59 +0545

    logout in user profile

--- jutta_junction/lib/pages/Profile/account_screen.dart
+++ jutta_junction/lib/pages/Profile/account_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:jutta_junction/pages/login_page.dart';
 import 'package:jutta_junction/viewmodels/auth_viewmodel.dart';
 import 'package:jutta_junction/viewmodels/global_ui_viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -393,6 +394,10 @@ class _AccountScreenState extends State<AccountScreen> {
         height: 60,
         child: ElevatedButton(
           onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => LoginScreen()));
+
             logout();
             // Add your code for logging out here
           },
