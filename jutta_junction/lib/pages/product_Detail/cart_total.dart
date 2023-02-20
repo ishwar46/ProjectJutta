486c0c265bea399b5f00fcaac7e653fddf88d8a8
sijan <sijankasalawat3339@gmail.com>
2023-02-20 22:02:01 +0545

    merging branch khaltipayment

--- jutta_junction/lib/pages/product_Detail/cart_total.dart
+++ jutta_junction/lib/pages/product_Detail/cart_total.dart
@@ -6,98 +6,95 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:jutta_junction/Controller/cart_controller.dart';
 
-class CartTotal
- extends StatelessWidget {
-  final CartController controller =Get.find();
-  
-CartTotal({Key?key}):super(key: key);
-  
+class CartTotal extends StatelessWidget {
+  final CartController controller = Get.find();
+
+  CartTotal({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return Obx(() =>
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20 ,vertical: 10),
-        
-        child:Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:<Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left:15,right:15),
-              child: Row(
-              mainAxisAlignment:MainAxisAlignment.spaceBetween,
-              children: [
-              
-                 Text("SUBTOTAL",style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-                  Text("${controller.total}",style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-              ],
-          ),
-            ),
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: Row(
-              mainAxisAlignment:MainAxisAlignment.spaceBetween,
-              children: [
-              
-                 Text("DELIVERY FEE",style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-                  Text("${200}",style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-              ],
-            ),
-          ),
-          Stack(
-            children: [
-              Container(
-                width:MediaQuery.of(context).size.width,
-                height:60,
-                decoration: BoxDecoration(
-                  
-                  color: Colors.grey,
-                 ),
-              ),
-             Padding(
-               padding: const EdgeInsets.all(20),
-               child: Row(
-                 mainAxisAlignment:MainAxisAlignment.spaceBetween,
-            children: [
-            
-                 Text("TOTAL",style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-                  Text( "${controller.sum}",style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-            ],
-               ),
-             )
-            ],
-          ),
-          
-           OutlinedButton(
-           style: OutlinedButton.styleFrom(
-    backgroundColor: Colors.black, 
-  ),
-
-            
-                            onPressed: (() {}), child: Text("Buy now",style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),))
-
-          ] 
-        )
-      ),
+    return Obx(
+      () => Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "SUBTOTAL",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "${controller.total}",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "DELIVERY FEE",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "${200}",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "TOTAL",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "${controller.sum}",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
+                    onPressed: (() {}),
+                    child: Text(
+                      "Buy now",
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                    ))
+              ])),
     );
   }
 }
