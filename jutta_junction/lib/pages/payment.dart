eb6097353d8ed126f095b73433caabe325640b43
ishwar46 <ishwarura@gmail.com>
2023-02-22 23:07:19 +0545

    Update payment.dart

--- jutta_junction/lib/pages/payment.dart
+++ jutta_junction/lib/pages/payment.dart
@@ -2,6 +2,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jutta_junction/customs/custom_icons.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Payment extends StatefulWidget {
   const Payment({Key? key}) : super(key: key);
@@ -11,163 +13,197 @@ class Payment extends StatefulWidget {
 }
 
 class _PaymentState extends State<Payment> {
-  String? payment;
+  String referenceId = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF90A4AE),
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 130,
-                      width: 20,
-                    ),
-                    CustomIcon(
-                      icon: Icons.arrow_back,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    const SizedBox(
-                      height: 130,
-                      width: 30,
-                    ),
-                    Container(
-                      child: const Text(
-                        "Payment Methods",
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.normal),
-                      ),
-                    ),
-                  ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new)
+              .iconColor(Colors.black)
+              .box
+              .make(),
+          onPressed: () {
+            Navigator.pushNamed(context, "/homepage");
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Payment Methods",
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Color.fromARGB(255, 92, 15, 163),
+                  fontWeight: FontWeight.bold,
                 ),
-                Container(
-                  margin: const EdgeInsets.all(30),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      //       Row(
-                      //         mainAxisAlignment: MainAxisAlignment.start,
-                      //         children: [
-                      //           TextButton(
-                      //             onPressed: () {},
-                      //             child: const Text(
-                      //               'Total Amount',
-                      //               style: TextStyle(color: Color.fromRGBO(
-                      //                   21, 34, 56, 1.0), fontSize: 17),
-                      //             ),
-                      //           )
-                      //         ],
-                      //       ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Divider(
+                color: Color.fromARGB(255, 92, 15, 163),
+                height: 20,
+                thickness: 1,
+              ),
+              SizedBox(
+                height: 20,
+              ),
 
-                      // Container(
-                      //   // margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                      //   decoration: BoxDecoration(
-                      //     color: Color.fromARGB(255, 230, 244, 252),
-                      //     borderRadius: BorderRadius.circular(30),
-                      //   ),
-                      //   child: TextFormField(
-                      //     keyboardType: TextInputType.number,
-                      //     inputFormatters: <TextInputFormatter>[
-                      //       FilteringTextInputFormatter.digitsOnly
-                      //     ],
-                      //     obscureText: false,
-                      //     decoration: const InputDecoration(
-                      //         border: InputBorder.none,
-                      //         // label: Text("Password"),
-                      //         hintText: "Enter Amount to Pay",
-                      //         prefixIcon: Icon(Icons.payment)),
-                      //   ),
-                      // ),
+              //Image for Khalti
+              InkWell(
+                splashColor: Colors.grey,
+                onTap: () {
+                  print("Image Tapped");
+                  khaltiWallet();
+                }, // Image tapped
+                child: Image.asset(
+                  'assets/images/khaltilogo.png',
+                  width: 180,
+                ),
+              ),
+
+              SizedBox(
+                height: 40,
+              ),
+
+              //Cash on Delivery
+              InkWell(
+                splashColor: Colors.grey,
+                onTap: () {
+                  AlertDialog(
+                    title: const Text("Cash on Delivery"),
+                    content: const Text("Payment successful"),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("OK"))
                     ],
-                  ),
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/cod.png',
+                  width: 100,
                 ),
-                RadioListTile(
-                  title: const Text(
-                    "Cash On Delivery",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  value: "COD",
-                  groupValue: payment,
-                  onChanged: (value) {
-                    setState(() {
-                      payment = value.toString();
-                    });
-                  },
-                ),
-                RadioListTile(
-                  title: const Text(
-                    "Esewa",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  value: "Esewa",
-                  groupValue: payment,
-                  onChanged: (value) {
-                    setState(() {
-                      payment = value.toString();
-                    });
-                  },
-                ),
-                RadioListTile(
-                  title: const Text(
-                    "Khalti",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  value: "Khalti",
-                  groupValue: payment,
-                  onChanged: (value) {
-                    setState(() {
-                      payment = value.toString();
-                    });
-                  },
-                ),
-                Container(
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              InkWell(
+                splashColor: Colors.grey,
+                onTap: () {
+                  AlertDialog(
+                    title: const Text("Cash on Delivery"),
+                    content: const Text("Payment successful"),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("OK"))
+                    ],
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/visa.png',
                   width: 150,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Navigator.of(context).pushReplacement(
-                      //   MaterialPageRoute(
-                      //     builder: (BuildContext context) => LoginScreen(),
-                      //   ),
-                      // );
-                    },
-                    style: ElevatedButton.styleFrom(
-                        primary: Color.fromARGB(255, 20, 79, 189),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        )),
-                    child: const Text(
-                      "Pay",
-                      style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 1.0),
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Divider(
+                color: Color.fromARGB(255, 92, 15, 163),
+                height: 20,
+                thickness: 1,
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  khaltiWallet() {
+    KhaltiScope.of(context).pay(
+      config: PaymentConfig(
+          amount: 1000,
+          productIdentity: "1",
+          productName: "nike blazers mid 77",
+          mobile: "9804704028"),
+      preferences: [PaymentPreference.khalti],
+      onSuccess: onSuccess,
+      onFailure: onFailure,
+      onCancel: onCanceled,
+    );
+  }
+
+  // mentod to handle success
+  void onSuccess(PaymentSuccessModel success) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Success"),
+            content: Text("Payment successful"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("OK"),
+              ),
+            ],
+          );
+        });
+  }
+
+  // method to handle failure
+  void onFailure(PaymentFailureModel failure) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Failure"),
+            content: Text("Payment failed"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("OK"))
+            ],
+          );
+        });
+  }
+
+  // method to handle onCanceled
+
+  void onCanceled() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Canceled"),
+          content: Text("Payment canceled"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("OK"))
+          ],
+        );
+      },
+    );
   }
 }
